be3d40375fbbe461d036860ed40a4534fb680656
David <davidaw.126@hotmail.com>
2021-10-30 02:36:16 -0400

    Modal for power use

--- static/game.js
+++ static/game.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	refreshSeat(true)
+	buildUp(false)
 	resetCard()
 	setInterval(refreshSeat,500);
 
@@ -21,7 +21,7 @@ function resetCard(){
 }
 
 
-function refreshSeat(buildUpGate){
+function refreshSeat(){
 	let formData = {};
 	let numberOfSeats = 0
 	$.ajax({
@@ -31,10 +31,7 @@ function refreshSeat(buildUpGate){
 	  dataType: "json",
 	  cache: false
 	}).done(function (data) {
-		numberOfSeats = data.numberOfSeats
-		if(buildUpGate){
-			buildUp(numberOfSeats)
-		}
+
 		  let roomTitleNumber = document.getElementById('roomTitleNumber');
 		  roomTitleNumber.innerHTML = "房间号： " + data.roomNumber
 		  for (let [key, value] of Object.entries(data.seats)) {
@@ -49,7 +46,6 @@ function refreshSeat(buildUpGate){
 			  	rolesString += key + " x " + value + "，";
 			  }
 		  }
-
 		  roles.innerHTML = "房间配置： " + rolesString.substring(0, rolesString.length - 1);
 
 	});
@@ -76,7 +72,7 @@ function reveal(){
 	}).done(function (data) {
 		console.log(data)
 		resetCard()
-		$('.modal').modal('show');
+		$('#reveal').modal('show');
 		let role = document.getElementById('insertRole');
 		role.innerHTML = Object.values(data)
 	});
@@ -97,44 +93,99 @@ function takeSeat(id){
 
 }
 
-function buildUp(numberOfSeats){
-	let body = document.getElementById('dynamic_insert');
-	for(let i = numberOfSeats ; i > 0; i--){
-		let newdiv = document.createElement('div');   //create a div
+function buildUp(lock){
+	$.ajax({
+	  type: "POST",
+	  url: "http://192.168.1.205:5000/seat/refresh",
+	  dataType: "json",
+	  cache: false
+	}).done(function (data) {
+		let numberOfSeats = data.numberOfSeats
 
-    	newdiv.id = i;                      //add an id
-		newdiv.className = 'four wide column'
+		let body = document.getElementById('dynamic_insert');
+		body.innerHTML = '';
+		for(let i = numberOfSeats ; i > 0; i--){
+			let newdiv = document.createElement('div');   //create a div
 
-		let labeled_button = document.createElement('dev');
-		labeled_button.type = 'button'
+			newdiv.id = i;                      //add an id
+			newdiv.className = 'four wide column'
 
-		labeled_button.setAttribute('onclick','takeSeat('+i+')')
-		labeled_button.className = 'ui left labeled button'
+			let labeled_button = document.createElement('dev');
+			labeled_button.type = 'button'
+			if(!lock) {
+				labeled_button.setAttribute('onclick', 'takeSeat(' + i + ')')
+			}else{
+				labeled_button.setAttribute('onclick', 'select(' + i + ')')
+			}
+			labeled_button.className = 'ui left labeled button'
 
-		let left_button =  document.createElement('a');
-		left_button.className = "ui grey right pointing label"
-		left_button.innerHTML = i
+			let left_button =  document.createElement('a');
+			left_button.className = "ui grey right pointing label"
+			left_button.innerHTML = i
 
-		let right_icon = document.createElement('i')
-		right_icon.className = "user icon"
+			let right_icon = document.createElement('i')
+			right_icon.className = "user icon"
 
-		let right_button = document.createElement('div')
-		right_button.className ="ui button squareButton"
-		right_button.appendChild(right_icon)
+			let right_button = document.createElement('div')
+			right_button.className ="ui button squareButton"
+			right_button.appendChild(right_icon)
 
-		let right_text = document.createElement('i')
-		right_text.id = "name" + i
-		right_text.innerHTML = "Empty"
-		right_button.appendChild(right_text)
+			let right_text = document.createElement('i')
+			right_text.id = "name" + i
+			right_text.innerHTML = "Empty"
+			right_button.appendChild(right_text)
 
-		labeled_button.appendChild(left_button)
-		labeled_button.appendChild(right_button)
+			labeled_button.appendChild(left_button)
+			labeled_button.appendChild(right_button)
 
 
-		newdiv.appendChild(labeled_button)
- 		body.appendChild(newdiv);
-		body.insertBefore(newdiv,body.firstChild)
-	}
+			newdiv.appendChild(labeled_button)
+			body.appendChild(newdiv);
+			body.insertBefore(newdiv,body.firstChild)
+		}
+
+
+	});
 
 }
 
+function startGame(){
+	let roomSettingList = []
+	$.ajax({
+	  type: "POST",
+	  url: "http://192.168.1.205:5000/seat/refresh",
+	  dataType: "json",
+	  cache: false
+	}).done(function (data) {
+		  for (let [key, value] of Object.entries(data.roomSettings)) {
+			  if(value > 0){
+			  	roomSettingList.push(key)
+			  }
+		  }
+		  roomSettingList = Array.from(new Set(roomSettingList))
+		  let order = ['盗贼','丘比特','野孩子','普通狼人','守卫','预言家','女巫','猎人','狼美人']
+		  const sortByObject  = data => data.reduce((obj,item,index) => { return { ...obj, [item]:index } }, {})
+		  let orderDict = sortByObject(order) /* {盗贼: 0, 丘比特: 1, 野孩子: 2} */
+
+		  let result = roomSettingList.sort((a, b) => orderDict[a] - orderDict[b])
+		  console.log(result)
+		  for(let i = 0;i<result.length;i++){
+
+		  }
+		  const music = new Audio('static/Howling.mp3');
+		  music.play();
+		  lockSeat()
+		  console.log(data)
+	});
+
+
+}
+
+function power(){
+	$('#power').modal('show');
+	console.log("hi")
+}
+
+function lockSeat(){
+	buildUp(true)
+}
